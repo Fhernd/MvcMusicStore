040b776b81389e90f34bb4a2b89cce87a1d0ab53
John Ortiz Ordoñez <johnortizo@outlook.com>
2016-04-03 22:38:39 -0500

    Ended Module 2 - Creating and Configuring Models.

---
+++ MvcMusicStore/Models/Artist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcMusicStore.Models
+{
+    public class Artist
+    {
+        public int ArtistID
+        {
+            get;
+            set;
+        }
+
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public List<Album> Albums
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MvcMusicStore/Models/Review.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID
+        {
+            get;
+            set;
+        }
+
+        public int AlbumID
+        {
+            get;
+            set;
+        }
+
+        public virtual Album Album
+        {
+            get;
+            set;
+        }
+
+        public string Contents
+        {
+            get;
+            set;
+        }
+
+        [Required()]
+        [DataType(DataType.EmailAddress)]
+        [DisplayAttribute(Name ="Email Address")]
+        public string ReviewerEmail
+        {
+            get;
+            set;
+        }
+    }
+}
